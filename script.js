ed937c3fba7c3f62d4b43d67721a9d3dfa72dfe5
BrunoCorreia-hub <correiabruno346@gmail.com>
2025-07-13 22:31:06 -0300

    adicionado o projeto devMovies e if else na hora de manda a mensagem

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function pegarValor() {
     const form = document.getElementById('formulario')
+    const mensagem = document.getElementById('mensagem-erro')
 
     const input = document.querySelector('.input-contato').value; // ou document.getElementById('nomeInput')
     const textArea = document.querySelector('.textarea-contato').value;
     const telefone = '61998032785';
+
+    if(input === '' || textArea === ''){
+      mensagem.textContent = "Por favor, preencha os campos antes de enviar";
+      return
+    } else {
+      mensagem.textContent = '';
+    }
 
     const textoFormatado = `Olá Bruno, me chamo ${input},%0A${textArea}`;
 
